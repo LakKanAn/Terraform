6c59ce7ddf31166bbc3d10bc6bb3fc5181efaf72
Jakkrapop Chivrarerk <jakkrapop@opsta.co.th>
2022-05-11 11:11:04 +0700

    update craete vm with public ip

--- variables.tf
+++ variables.tf
@@ -1,13 +1,9 @@
-variable "user_public_key" {
-  type = string
-  default = ""
-  sensitive = true
-  description = "user public key"
+variable "resource_group_name_prefix" {
+  default       = "rg"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-variable "location" {
-  type = string
-  default = "Southeast Asia"
-  sensitive = true
-  description = "resource group location"
+variable "resource_group_location" {
+  default       = "eastus"
+  description   = "Location of the resource group."
 }
